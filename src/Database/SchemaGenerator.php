9dd187004676239f1016cb91baceb12c27062442
nonameebyrator985 <singheingemo@hotmail.com>
2025-08-15 20:53:26 +0900

    feat: initial commit for src/Database/SchemaGenerator.php

---
+++ src/Database/SchemaGenerator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MigrationLegacyCode\Database;
+
+class SchemaGenerator {
+    public function generate() {
+        // Logique pour générer des schémas de base de données modernisés
+    }
+}
